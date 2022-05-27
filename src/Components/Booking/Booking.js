c7e3f9892c621708322070b83848f9da5a51e332
unknown <rhrabin934602@gmail.com>
2022-05-27 23:49:21 +0600

    booking page work and set modal

--- src/Components/Booking/Booking.js
+++ src/Components/Booking/Booking.js
@@ -10,10 +10,6 @@ import BookingData from './BookingData';
 
 const Booking = () => {
     const { id } = useParams();
-    const [modalData, setModalData] = useState(null);
-
-    let iconStyles = { color: "red", fontSize: "1.5em", marginTop: '5px' };
-    const [quantityOrder, setQuantityOrder] = useState(12);
 
     const { data: product, isLoading, refetch } = useQuery('service', () => fetch(`https://peaceful-waters-42797.herokuapp.com/service/${id}`, {
         method: 'GET',
@@ -28,28 +24,6 @@ const Booking = () => {
     }
 
 
-    // console.log(quantityOrder, product?.minimumQuantity)
-
-    // load single service
-
-
-
-
-    const handleMinus = () => {
-        setQuantityOrder(quantityOrder - 1);
-        if (quantityOrder <= 13) {
-            toast.error("You can't book less then 12")
-        }
-    }
-    const handlePlus = () => {
-        setQuantityOrder(quantityOrder + 1);
-        if (quantityOrder >= product.quantity) {
-            toast.error(`You can't book more than ${product.qunatity}`)
-        }
-    }
-    const openModal = () => {
-        setModalData(1);
-    }
 
 
     return (
@@ -57,52 +31,19 @@ const Booking = () => {
 
 
         <div>
-            <div className="overflow-x-auto mt-12 px-1 lg:px-24">
-                <table className="table w-full ">
+            <BookingData
+                key={product._id}
+                product={product}
+                id={id}
+                refetch={refetch}
 
-                    <thead className='text-purple-300'>
-                        <tr>
-                            <th>SL</th>
-                            <th>Name</th>
-                            <th>Stocks</th>
-                            <th>Minumum Order</th>
-                            <th>price</th>
-                        </tr>
-                    </thead>
-                    <tbody>
+            ></BookingData>
 
-                        <tr>
-                            <td className='text-red-400 font-bold'>1</td>
-                            <td className='text-red-400 font-bold'>{product?.name}</td>
-                            <td className='text-red-400 font-bold'>{product?.quantity}</td>
-                            <td className='text-red-400 font-bold'><span className='font-bold text-center'>
-                                <button disabled={quantityOrder <= 12} className='btn  btn-xs mr-2 btn-success' onClick={handleMinus}> <AiOutlineMinusCircle style={iconStyles} /> </button>
-
-                                <input type="text" name='order' className='w-12 bg-black text-white' value={quantityOrder} />
-
-                                <button disabled={quantityOrder >= product.quantity} className='btn btn-success btn-xs ml-2' onClick={handlePlus}><AiOutlinePlusCircle style={iconStyles} /></button></span></td>
-
-                            <td className='text-red-400 font-bold'>$ {product?.price}</td>
-                        </tr>
-
-                    </tbody>
-                </table>
-            </div>
-            {/* <button onClick={handleOrder} className='btn'>click</button> */}
-            <div className='text-center mt-10'>
+            {/* <div className='text-center mt-10'>
                 <label onClick={openModal} for="bookingModals" className="btn  btn-info">Order Now</label>
-            </div>
-            <BookingData quantity={product.quantity}></BookingData>
-            {
-                modalData && <BookinModal
-                    key={product._id}
-                    product={product}
-                    refetch={refetch}
-                    id={id}
-                    quantityOrder={quantityOrder}
-                    setModalData={setModalData}
-                ></BookinModal>
-            }
+            </div> */}
+
+            <BookinModal></BookinModal>
 
         </div >
 
--- src/Components/Booking/BookingData.js
+++ src/Components/Booking/BookingData.js
@@ -1,18 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { AiOutlineMinus, AiOutlinePlus, AiOutlinePlusCircle } from 'react-icons/ai'
+import { AiOutlineMinusCircle } from 'react-icons/ai';
+import BookinModal from './BookinModal';
+const BookingData = ({ product, id, refetch, }) => {
+    const { picture, quantity, price, name, description, minimumQuantity, } = product;
+    const [inputQuantity, setInputQuantity] = useState(minimumQuantity);
+    const [totalPrice, setTotalPrice] = useState(minimumQuantity * price);
+    const [modalData, setModalData] = useState(null);
+    const openModal = () => {
+        setModalData(1);
+    }
 
-const BookingData = ({ quantity }) => {
     return (
         <div>
-            {console.log(quantity)}
+            {console.log(inputQuantity)}
             <div className="hero min-h-screen bg-base-200">
                 <div className="hero-content flex-col lg:flex-row">
-                    <img src="https://api.lorem.space/image/movie?w=260&h=400" className="max-w-sm rounded-lg shadow-2xl" />
+                    <img src={picture} className="max-w-sm rounded-lg shadow-2xl" alt='pictures' />
                     <div>
-                        <h1 className="text-5xl font-bold">Box Office News!</h1>
-                        <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                        <button className="btn btn-primary">Get Started</button>
+                        <h1 className="text-5xl font-bold">{name}</h1>
+                        <p className="py-6">{description}</p>
+                        <div className='flex lg:mt-6'>
+                            <p className='bg-cyan-200 p-2 lg:px-6  text-red-400' >Total Stocks: {quantity}</p>
+                            <p className='ml-4 bg-cyan-200  p-2 text-red-400' >Minimum Order Quantity: {minimumQuantity}</p>
+
+                        </div>
+                        <div className='flex'>
+                            <p className='bg-sky-100 p-2 text-red-400 mt-2 text-center font-bold mr-2'>Price Per Product: ${price}</p>
+                            <p className='bg-sky-100 p-2 text-red-400 mt-2 text-center font-bold ml-2'>Total Price: ${totalPrice}</p>
+
+                        </div>
+
+                        <div className='flex justify-center items-center'>
+                            <button disabled={inputQuantity <= minimumQuantity} onClick={() => {
+                                setInputQuantity(inputQuantity - 1)
+                                setTotalPrice(inputQuantity * price)
+                            }} className='btn mt-4 btn-sm btn-outline btn-info mr-4' ><AiOutlineMinus style={{ fontSize: '2.0em', }} /></button>
+
+                            <input className='mt-4 w-20 text-center text-xl text-red-500' value={inputQuantity} type="text" name="quantity" id="quantity" />
+
+                            <button onClick={() => {
+                                setInputQuantity(inputQuantity + 1)
+                                setTotalPrice(inputQuantity * price)
+                            }} className='btn mt-4 btn-sm btn-outline btn-info ml-4'><AiOutlinePlus style={{ fontSize: '2.0em' }} /></button>
+                        </div>
+
+                        {
+                            modalData && <BookinModal
+                                key={product._id}
+                                product={product}
+                                refetch={refetch}
+                                id={id}
+                                quantityOrder={inputQuantity}
+                                setModalData={setModalData}
+                            ></BookinModal>
+                        }
                     </div>
+
                 </div>
+            </div>
+            <div className='text-center mt-10'>
+                <label onClick={openModal} for="bookingModals" className="btn  btn-info">Order Now</label>
             </div>
         </div>
     );
