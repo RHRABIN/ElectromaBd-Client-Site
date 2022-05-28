11e41c59a7742f34cf9759b0a894f5e01cd63a5a
unknown <rhrabin934602@gmail.com>
2022-05-28 12:48:06 +0600

    home page animation added

--- src/Components/Home/Parts.js
+++ src/Components/Home/Parts.js
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import SingleParts from './SingleParts';
-import bg1 from '../../assests/bg1.jpg'
-import { useNavigate } from 'react-router-dom';
 import useAllParts from '../../hooks/UseAllParts';
+import LightSpeed from 'react-reveal/LightSpeed';
 
 const Parts = () => {
     const [products, setProducts] = useAllParts()
@@ -13,7 +12,7 @@ const Parts = () => {
     // }
     return (
         <div className='mb-12 '>
-            <h2 className="text-3xl text-center mt-20 mb-12 text-purple-500 font-bold ">Our Product Collections</h2>
+            <h2 className="text-3xl text-center mt-20 mb-12 text-purple-500 font-bold "><LightSpeed left cascade>Our Product Collections</LightSpeed></h2>
             <div className='grid sm:grid-cols-1 lg:grid-cols-4 gap-5 px-4 lg:px-20'>
                 {
                     products.slice().map(product => <SingleParts
--- src/Components/Home/SingleParts.js
+++ src/Components/Home/SingleParts.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import bg1 from '../../assests/bg1.jpg'
+import Fade from 'react-reveal/Fade'
 const SingleParts = ({ product }) => {
     const { name, quantity, minimumQuantity, price, description, picture, _id, badge } = product;
     const navigate = useNavigate()
@@ -8,22 +9,24 @@ const SingleParts = ({ product }) => {
         navigate(`/booking/${id}`)
     }
     return (
-        <div className="card  shadow-2xl ">
-            <figure><img className='object-cover h-40 ' src={picture} alt={name} /></figure>
-            <div className="card-body " >
-                <h2 className="card-title">
-                    {name}
-                    {/* <div className="badge badge-warning">{badge}</div> */}
-                </h2>
-                <p>{description}</p>
-                <div className="card-actions justify-center">
-                    <div className="badge badge-outline bg-red-600 text-white">Available: {quantity}</div>
-                    <div className="badge badge-outline bg-red-600 text-white ">Minimum Order: {minimumQuantity}</div>
-                    <div className="badge badge-outline bg-red-500 text-white">Price: ${price}</div>
+        <Fade bottom >
+            <div className="card  shadow-2xl ">
+                <figure><img className='object-cover h-40 ' src={picture} alt={name} /></figure>
+                <div className="card-body " >
+                    <h2 className="card-title">
+                        {name}
+                        {/* <div className="badge badge-warning">{badge}</div> */}
+                    </h2>
+                    <p>{description}</p>
+                    <div className="card-actions justify-center">
+                        <div className="badge badge-outline bg-red-600 text-white">Available: {quantity}</div>
+                        <div className="badge badge-outline bg-red-600 text-white ">Minimum Order: {minimumQuantity}</div>
+                        <div className="badge badge-outline bg-red-500 text-white">Price: ${price}</div>
+                    </div>
+                    <button onClick={() => handleBooking(_id)} className='btn btn-outline btn-info font-bold'>Book Now</button>
                 </div>
-                <button onClick={() => handleBooking(_id)} className='btn btn-outline btn-info font-bold'>Book Now</button>
             </div>
-        </div>
+        </Fade>
     );
 };
 
